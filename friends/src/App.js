ee94c67cc8db7129dd025e953e984185e14260c1
Nicholas Pung <nicholaspung>
2019-06-12 15:42:07 -0700

    update functionality works but doesnt show up on screen, still need to figure out how to re-render the page to update the updated objects

--- friends/src/App.js
+++ friends/src/App.js
@@ -9,50 +9,38 @@ class App extends React.Component {
     friends: [],
     name: '',
     age: '',
-    email: '',
-    id: '',
-    mapId: ''
+    email: ''
   }
 
   componentDidMount() {
-    // passes in something to initiate mapId to friends length synchronously
-    this.grabFriendData(1)
-  }
-
-  grabFriendData = (id = 0) => {
     axios.get('http://localhost:5000/friends')
       .then(res => this.setState({ friends: res.data }))
-      .then(e => {
-        // code is only used to initiate mapId to friends length synchronously, no other use case was thought of for this code
-        if (id) {this.setState({ mapId: this.state.friends.length+1})}
-      })
       .catch(err => console.log('Error', err))
   }
 
   onInputChange = e => {
     this.setState({ [e.target.name]: e.target.value })
-}
+  }
 
   addNewFriend = e => {
     e.preventDefault();
     axios.post('http://localhost:5000/friends', {
       name: this.state.name,
       age: parseInt(this.state.age),
-      email: this.state.email,
-      id: this.state.mapId
+      email: this.state.email
     })
-      .then(res => {
-        this.setState(prevState => ({ mapId: prevState.mapId+1 }))
-        return this.grabFriendData()
-      })
+      .then(res => this.setState({ 
+        friends: res.data,
+        name: '',
+        age: '',
+        email: ''
+      }))
       .catch(err => console.log('Error', err))
   }
 
   deleteFriend = id => {
     axios.delete(`http://localhost:5000/friends/${id}`)
-      .then(res => {
-        return this.grabFriendData()
-      })
+      .then(res => this.setState({ friends: res.data }))
       .catch(err => console.log(err.message))
   }
 
@@ -60,8 +48,8 @@ class App extends React.Component {
     return (
       <div className="App">
         Hello
-        <AddFriend addNewFriend={this.addNewFriend} onInputChange={this.onInputChange} title='Add a Friend!'/>
-        <FriendList friends={this.state.friends} updateFriend={this.updateFriend} deleteFriend={this.deleteFriend}/>
+        <AddFriend onSubmitForm={this.addNewFriend} onInputChange={this.onInputChange} title="Add a New Friend!" state={this.state} />
+        <FriendList friends={this.state.friends} deleteFriend={this.deleteFriend}/>
       </div>
     );
   }
--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -1,15 +1,47 @@
 import React from 'react';
+import AddFriend from './AddFriend';
+import axios from 'axios';
 
-const Friend = props => {
-    return (
-        <div>
-            <p>
-                {props.friend.name}, {props.friend.age}
-                <button onClick={() => props.deleteFriend(props.friend.id)}>delete friend</button>
-            </p>
-            <p>{props.friend.email}</p>
-        </div>
-    );
+class Friend extends React.Component {
+    state = {
+        toggleButton: false,
+        name: this.props.friend.name,
+        age: this.props.friend.age,
+        email: this.props.friend.email
+    }
+
+    changeToggle = () => {
+        this.setState(prevState => ({ toggleButton: !prevState.toggleButton }))
+    }
+
+    updateFriend = (id, e) => {
+        e.preventDefault();
+        axios.put(`http://localhost:5000/friends/${id}`, {
+            name: this.state.name,
+            age: this.state.age,
+            email: this.state.email
+        })
+            .then(res => console.log(res))
+            .catch(err => console.log('Error', err))
+    }
+
+    onInputChange = e => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    render() {
+        console.log(this.state.name)
+        return (
+            <div>
+                <p>
+                    {this.props.friend.name}, {this.props.friend.age}
+                    <button onClick={() => this.props.deleteFriend(this.props.friend.id)}>delete friend</button>
+                </p>
+                <p>{this.props.friend.email}</p>
+                {this.state.toggleButton ? <AddFriend state={this.state} onInputChange={this.onInputChange} changeToggle={this.changeToggle} onSubmitForm={e => this.updateFriend(this.props.friend.id, e)}/> : <button onClick={this.changeToggle}>update friend</button>}
+            </div>
+        );
+    }
 }
 
 export default Friend;
